5816828084654cc0e5a6d1270a03d5c4ac42d06c
Dida <kandida003@gmail.com>
2023-04-21 13:48:32 +0700

    features

--- src/data/features-data.js
+++ src/data/features-data.js
@@ -7,21 +7,28 @@ import {
 export const featuresData = [
   {
     color: "blue",
-    title: "Awarded Agency",
+    title: "Training & Sertifikasi",
     icon: StarIcon,
     description:
       "Divide details about your product or agency work into parts. A paragraph describing a feature will be enough.",
   },
   {
     color: "red",
-    title: "Free Revisions",
+    title: "Human Resource & Employee Tools",
     icon: ArrowPathIcon,
     description:
       "Keep you user engaged by providing meaningful information. Remember that by this time, the user is curious.",
   },
   {
     color: "teal",
-    title: "Verified Company",
+    title: "[On Development] Dhifarindo Trading & Wisata",
+    icon: FingerPrintIcon,
+    description:
+      "Write a few lines about each one. A paragraph describing a feature will be enough. Keep you user engaged!",
+  },
+  {
+    color: "green",
+    title: "[On Development] Research Gate Dhifarindo Institute",
     icon: FingerPrintIcon,
     description:
       "Write a few lines about each one. A paragraph describing a feature will be enough. Keep you user engaged!",
