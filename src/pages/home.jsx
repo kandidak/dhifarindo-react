22eeddba06e4d01a215c424a5079e574e5a8ee69
Dida <kandida003@gmail.com>
2023-04-22 08:44:52 +0700

    halaman profile

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,7 +12,7 @@ import {
 import { UsersIcon } from "@heroicons/react/24/solid";
 import { PageTitle, Footer } from "@/widgets/layout";
 import { FeatureCard, TeamCard } from "@/widgets/cards";
-import { featuresData, teamData, contactData } from "@/data";
+import { featuresData, teamData } from "@/data";
 import { useForm } from "react-hook-form";
 import { addDoc, collection } from "@firebase/firestore";
 import { firestore } from "../firebase_setup/firebase";
@@ -187,50 +187,6 @@ export function Home() {
               />
             ))}
           </div>
-        </div>
-      </section>
-      <section className="relative bg-blue-gray-50/50 px-4 py-24">
-        <div className="container mx-auto">
-          <PageTitle heading="Build something">
-            Put the potentially record low maximum sea ice extent tihs year down
-            to low ice. According to the National Oceanic and Atmospheric
-            Administration, Ted, Scambos.
-          </PageTitle>
-          <div className="mx-auto mb-48 mt-20 grid max-w-5xl grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3">
-            {contactData.map(({ title, icon, description }) => (
-              <Card
-                key={title}
-                color="transparent"
-                shadow={false}
-                className="text-center text-blue-gray-900"
-              >
-                <div className="mx-auto mb-6 grid h-14 w-14 place-items-center rounded-full bg-white shadow-lg shadow-gray-500/20">
-                  {React.createElement(icon, {
-                    className: "w-5 h-5",
-                  })}
-                </div>
-                <Typography variant="h5" color="blue-gray" className="mb-2">
-                  {title}
-                </Typography>
-                <Typography className="font-normal text-blue-gray-500">
-                  {description}
-                </Typography>
-              </Card>
-            ))}
-          </div>
-          <PageTitle heading="Want to work with us?">
-            Complete this form and we will get back to you in 24 hours.
-          </PageTitle>
-          <form className="mx-auto mt-12 max-w-3xl text-center">
-            <div className="mb-8 flex gap-8">
-              <Input variant="standard" size="lg" label="Full Name" />
-              <Input variant="standard" size="lg" label="Email Address" />
-            </div>
-            <Textarea variant="standard" size="lg" label="Message" rows={8} />
-            <Button variant="gradient" size="lg" className="mt-8">
-              Send Message
-            </Button>
-          </form>
         </div>
       </section>
       <div className="bg-blue-gray-50/50">
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,10 +1,12 @@
-import { Avatar, Typography, Button } from "@material-tailwind/react";
+import React from "react";
+import { Card, Typography, Button, Input, Textarea } from "@material-tailwind/react";
 import {
   MapPinIcon,
   BriefcaseIcon,
   BuildingLibraryIcon,
 } from "@heroicons/react/24/solid";
-import { Footer } from "@/widgets/layout";
+import { contactData } from "@/data";
+import { PageTitle, Footer } from "@/widgets/layout";
 
 export function Profile() {
   return (
@@ -59,19 +61,57 @@ export function Profile() {
               <div className="mb-10 border-t border-blue-gray-50 py-6 text-center">
                 <div className="mt-2 flex flex-wrap justify-center">
                   <div className="flex w-full flex-col items-center px-4 lg:w-9/12">
-                    <Typography className="mb-8 font-normal text-blue-gray-500">
-                      An artist of considerable range, Jenna the name taken by
-                      Melbourne-raised, Brooklyn-based Nick Murphy writes,
-                      performs and records all of his own music, giving it a
-                      warm, intimate feel with a solid groove structure. An
-                      artist of considerable range.
-                    </Typography>
-                    <Button variant="text">Show more</Button>
+                    <img src="/img/brosur.jpg" />
                   </div>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+      </section>
+      <section className="relative bg-blue-gray-50/50 px-4 py-24">
+        <div className="container mx-auto">
+          <PageTitle heading="Build something">
+            Put the potentially record low maximum sea ice extent tihs year down
+            to low ice. According to the National Oceanic and Atmospheric
+            Administration, Ted, Scambos.
+          </PageTitle>
+          <div className="mx-auto mb-48 mt-20 grid max-w-5xl grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3">
+            {contactData.map(({ title, icon, description }) => (
+              <Card
+                key={title}
+                color="transparent"
+                shadow={false}
+                className="text-center text-blue-gray-900"
+              >
+                <div className="mx-auto mb-6 grid h-14 w-14 place-items-center rounded-full bg-white shadow-lg shadow-gray-500/20">
+                  {React.createElement(icon, {
+                    className: "w-5 h-5",
+                  })}
+                </div>
+                <Typography variant="h5" color="blue-gray" className="mb-2">
+                  {title}
+                </Typography>
+                <Typography className="font-normal text-blue-gray-500">
+                  {description}
+                </Typography>
+              </Card>
+            ))}
+          </div>
+          <PageTitle heading="Want to work with us?">
+            Complete this form and we will get back to you in 24 hours.
+          </PageTitle>
+          <form className="mx-auto mt-12 max-w-3xl text-center">
+            <div className="mb-8 flex gap-8">
+              <Input variant="standard" size="lg" label="Full Name" required />
+              <Input variant="standard" size="lg" label="Email Address" required />
+              <Input variant="standard" size="lg" label="Phone Number" required />
+            </div>
+            <Textarea variant="standard" size="lg" label="Message" rows={8} />
+            <Button variant="gradient" size="lg" className="mt-8">
+              Send Message
+            </Button>
+          </form>
         </div>
       </section>
       <div className="bg-blue-gray-50/50">
