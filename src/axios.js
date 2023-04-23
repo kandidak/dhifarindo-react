4e161abbccefdcc25128d84fc7789ce03d873872
andi <andi@balicipta.com>
2023-04-23 20:25:31 +0700

    update package

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,31 @@
 import axios from 'axios';
 
-const instance = axios.create({
-    baseURL : 'firebase-adminsdk-ti4cb@skripsi-a7a73.iam.gserviceaccount.com'
+// const instance = axios.create({
+//     baseURL : 'http:localhost:8800',
+//     headers : {'X-Requested-With' : 'XMLHttpRequest'},
+//     withCredentials:true
+// });
+
+axios.defaults.baseURL = 'http:localhost:8800';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(request => {
+    console.log(request);
+    // Edit request config
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
 });
 
-export default instance;
+axios.interceptors.response.use(response => {
+    console.log(response);
+    // Edit response config
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+export default axios;
